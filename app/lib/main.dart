fcc0531ec2c56720237b1fceba776d0be2ca98d6
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-04-06 20:17:33 +0530

    feat(app): init theme, color scheme

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:app/intro_page/views/intro_page_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const HealthSync());
-}
+import 'package:app/src/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HealthSync extends StatelessWidget {
-  const HealthSync({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HealthSync',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: IntroPage(),
-    );
-  }
+void main(List<String> args) async {
+  runApp(const ProviderScope(child: HealthSync()));
 }
