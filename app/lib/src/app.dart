c2c077b8cb25a426f464e42a8020de4775a0aa80
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-04-06 20:17:42 +0530

    chore(app): rename files

---
+++ app/lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:app/src/navigation/views/navigation_view.dart';
+
+class HealthSync extends StatelessWidget {
+  const HealthSync({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HealthSync',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const NavigationView(),
+    );
+  }
+}
